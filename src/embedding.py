c9a53ca7b12576b533849fdb9ccdaaa3428e7783
leandermaerkisch <leander.maerkisch@floy.com>
2024-06-29 13:57:36 -0700

    Add embedding module

--- src/embedding.py
+++ src/embedding.py
@@ -1,7 +1,17 @@
 import os
 from dotenv import load_dotenv
+import voyageai
 
 load_dotenv()
 
 VOYAGE_API_KEY = os.getenv("VOYAGE_API_KEY")
 
+vo = voyageai.Client()
+
+result = vo.embed(["hello world"], model="voyage-large-2-instruct")
+
+documents_embeddings = vo.embed(
+    texts=["hello world"], model="voyage-law-2", input_type="document"
+).embeddings
+
+print(documents_embeddings)
