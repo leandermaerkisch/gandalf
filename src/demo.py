a2ebdaf5e092ed5551a7aa4cde8538a683cd7a1d
leandermaerkisch <leander.maerkisch@floy.com>
2024-06-29 19:25:14 -0700

    Working hardcoded frontent

--- src/demo.py
+++ src/demo.py
@@ -3,9 +3,10 @@ from gradio_pdf import PDF
 from pathlib import Path
 
 dir_ = Path(__file__).parent
+AUDIO_FILE = "assets/gandalf_speech.mp3"
 
-def inference(doc: str) -> tuple:
-    return "https://gradio-builds.s3.amazonaws.com/assets/pdf-guide/fw9.pdf"
+def inference(doc: str) -> str:
+    return "assets/demo_result.pdf"
 
 def download_pdf():
     return "https://gradio-builds.s3.amazonaws.com/assets/pdf-guide/fw9.pdf"
@@ -15,16 +16,17 @@ with gr.Blocks() as demo:
     input_pdf = PDF(label="Document")
     output_pdf = PDF(label="Document")
 
-    audio_file = "assets/gandalf_speech.mp3"
-    audio_button = gr.Audio(audio_file, autoplay=False, label="Play Audio")
-    
+    print(f"Input PDF: {input_pdf}")
+
+    audio_button = gr.Audio(AUDIO_FILE, autoplay=False, label="Play Audio")
+
     gr.Interface(
         fn=inference,
         inputs=[input_pdf],
         outputs=[output_pdf],
     )
 
-    download_button = gr.DownloadButton("Download PDF", value=download_pdf())
+    download_button = gr.DownloadButton("Download PDF", value=download_pdf)
 
 if __name__ == "__main__":
-    demo.launch()
+    demo.launch(debug=True)
